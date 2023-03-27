c292e2ed8429a386f3c3f5364aae0911627ff234
PedroMoreira-a22002701 <pedromrm2001@gmail.com>
2023-03-27 15:21:08 +0100

    11?

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -2,10 +2,14 @@ package pt.ulusofona.cm.kotlin.challenge.models
 
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 
 class Carro(override val identificador: String, val motor: Motor) : Veiculo(identificador), Movimentavel {
     override fun toString(): String {
-        return "Carro | ${identificador} | ${dataDeAquisicao} | "+ posicao.toString()
+        val formatoData = SimpleDateFormat("dd-MM-yyyy")
+        val dataFormatada = formatoData.format(dataDeAquisicao)
+        return "Carro | ${identificador} | ${dataFormatada} | "+ posicao.toString()
     }
 
     override fun requerCarta(): Boolean {
--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -5,7 +5,6 @@ import java.util.Date
 abstract class Veiculo(open val identificador: String) {
     var posicao: Posicao = Posicao(0,0)
     var dataDeAquisicao: Date = Date()
-
     abstract fun requerCarta(): Boolean
 
 }
