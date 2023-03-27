3aa12019003feaa10896bc3f7891322948ff33ab
PedroMoreira-a22002701 <pedromrm2001@gmail.com>
2023-03-27 16:04:06 +0100

    15?

--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -2,9 +2,11 @@ package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.interfaces.*
 import java.util.Date
 
-abstract class Veiculo(open val identificador: String): Ligavel, Movimentavel {
+abstract class Veiculo(open val identificador: String): Movimentavel {
     var posicao: Posicao = Posicao(0,0)
     var dataDeAquisicao: Date = Date()
     abstract fun requerCarta(): Boolean
-
+    override fun moverPara(x: Int, y: Int) {
+        posicao.alterarPosicaoPara(x,y)
+    }
 }
