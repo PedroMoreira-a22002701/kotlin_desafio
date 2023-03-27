87b7aad07b9bf091f52178133ed34edab59d6981
PedroMoreira-a22002701 <pedromrm2001@gmail.com>
2023-03-27 15:59:51 +0100

    15?

--- src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
@@ -1,14 +1,18 @@
 package pt.ulusofona.cm.kotlin.challenge.models;
+import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
 import java.text.SimpleDateFormat
 
-class Bicicleta(override var identificador: String) : Veiculo(identificador), Movimentavel{
+abstract class Bicicleta(override var identificador: String) : Veiculo(identificador), Movimentavel, Ligavel{
     override fun toString(): String {
         val formatoData = SimpleDateFormat("dd-MM-yyyy")
         val dataFormatada = formatoData.format(dataDeAquisicao)
         return "Bicicleta | ${identificador} | ${dataFormatada} | "+ posicao.toString()
     }
+    abstract override fun ligar()
+    abstract override fun desligar()
 
+    abstract override fun estaLigado(): Boolean
     override fun requerCarta(): Boolean {
         return false
     }
